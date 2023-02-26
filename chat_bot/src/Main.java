0582033cc831535cb37066e8d4b9078d753c23b2
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-02-26 18:58:01 +0530

    chat bot Demo

---
+++ chat_bot/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // work with chatbot
+        System.out.println("Hello world!");
+        ChatBot chatBot = new ChatBot();
+        chatBot.intractionWithHuman();
+    }
+}
