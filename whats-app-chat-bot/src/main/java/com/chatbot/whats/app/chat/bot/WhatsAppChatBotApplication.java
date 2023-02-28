cf33c5a15eb41630595c18480965121a5edc76c7
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-02-28 23:00:45 +0530

    chat bot updated with mongodb
    whats app chat bot by using java spring boot with  mongoDb

---
+++ whats-app-chat-bot/src/main/java/com/chatbot/whats/app/chat/bot/WhatsAppChatBotApplication.java
@@ -0,0 +1,13 @@
+package com.chatbot.whats.app.chat.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsAppChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsAppChatBotApplication.class, args);
+	}
+
+}
---
+++ whats-app-chat-bot/src/test/java/com/chatbot/whats/app/chat/bot/WhatsAppChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatbot.whats.app.chat.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
